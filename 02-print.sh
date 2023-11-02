fa90e0e4eeed171cc6a93d1ea75531cebc61bfde
Anto <japanto20@gmail.com>
2023-11-02 14:24:20 -0500

    Create servers

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,18 @@
 echo hello world
+
+# color syntax: echo -e "\e[COLm Message \e[0m"
+
+
+# echo -e , -e is to allow colors
+# Double quotes are mandatory, Single quotes can be used but not preferred.
+# \e[COLm -> COL is a color code we need provide
+  # RED       - 31
+  # GREEN     - 32
+  # YELLOW    - 33
+  # BLUE      - 34
+  # MAGENTA   - 35
+  # CYAN      - 36
+# \e[0m -> THis is to disable the enabled color
+
+echo -e "\e[31m This text in Red Color \e[0m"
+echo -e "\e[33m This text in Yellow Color \e[0m"
